c9f9f7a74592f13b8b7743426a7a2ec53f0d251e
cydl <cydl@ES00008482.petrobras.biz>
2016-10-13 16:46:05 -0300

    alteração

--- teste.js
+++ teste.js
@@ -1,1 +1,19 @@
-document.write("hello world");
+function incluir() {
+	var nome = document.getElementById("nome").value;
+	var tabela = document.getElementById("pessoas");
+	var linha = document.createElement("tr");
+	
+	var colName = document.createElement("td");
+	colName.appendChild(document.createTextNode(nome));
+	linha.appendChild(colName);
+	
+	var colEndereco = document.createElement("td");
+	colEndereco.appendChild(document.createTextNode("jardim da penha"));
+	linha.appendChild(colEndereco);
+	
+	var colTelefone = document.createElement("td");
+	colTelefone.appendChild(document.createTextNode("(71) 3248-8349"));
+	linha.appendChild(colTelefone);
+	
+	tabela.appendChild(linha);
+}
