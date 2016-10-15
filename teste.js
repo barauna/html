6320fc2a3d6708e115149d9296c784b3051db3dc
Familia <Familia@barah-desktop.home>
2016-10-15 20:47:15 -0300

    inclusao css

--- teste.js
+++ teste.js
@@ -1,5 +1,18 @@
+function limpar(){
+	var inputs = document.getElementsByTagName("input");
+	for (var x = 0; x < inputs.length; x++) {
+		if (inputs[x].type != "button"){
+			inputs[x].value = "";
+		}
+	}
+}
+
 function incluir() {
+	
 	var nome = document.getElementById("nome").value;
+	var endereco = document.getElementById("endereco").value;
+	var telefone = document.getElementById("telefone").value;
+	
 	var tabela = document.getElementById("pessoas");
 	var linha = document.createElement("tr");
 	
@@ -8,12 +21,25 @@ function incluir() {
 	linha.appendChild(colName);
 	
 	var colEndereco = document.createElement("td");
-	colEndereco.appendChild(document.createTextNode("jardim da penha"));
+	colEndereco.appendChild(document.createTextNode(endereco));
 	linha.appendChild(colEndereco);
-	
+
 	var colTelefone = document.createElement("td");
-	colTelefone.appendChild(document.createTextNode("(71) 3248-8349"));
+	colTelefone.appendChild(document.createTextNode(telefone));
 	linha.appendChild(colTelefone);
 	
+	var imagem = document.createElement("img");
+	imagem.setAttribute("src", "images/exclude.jpg");
+	
+	var colExcluir = document.createElement("td");
+	colExcluir.appendChild(imagem);
+	linha.appendChild(colExcluir);
+
+	
 	tabela.appendChild(linha);
+	
+	limpar();
+	
+	document.getElementById("nome").focus();
+	
 }
